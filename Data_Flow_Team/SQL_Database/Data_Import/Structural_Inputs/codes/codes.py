cbaf4a29ff911ac1f11aa32108182f42641a6171
Aurelie <amp0028@tigermail.auburn.edu>
2019-02-26 08:24:55 -0500

    Import Codes into Database

--- Data_Flow_Team/SQL_Database/Data_Import/Structural_Inputs/codes/codes.py
+++ Data_Flow_Team/SQL_Database/Data_Import/Structural_Inputs/codes/codes.py
@@ -1,13 +1,45 @@
 # -*- coding: utf-8 -*-
 """ The objective of this script is to import all possible unique 3-letter farm codes into the CROWN postgresql database. """
 
+# ----- Preliminary Coding -----
+
+# import required packages
+
+import os  # to define work directory
 import psycopg2 # import required module to connect to postgresql database
 import csv
 
-# define function to insert all possible unique 3-letter codes into the CROWN database "codes" table
-def import_codes(code_list):
+# set work directory
+os.chdir("/Users/amponcet/Documents/GitHub/CROWN/Data_Flow_Team/SQL_Database/Data_Import/Structural_Inputs/codes") 
+print(os.getcwd()) # print current work directory
 
-    sql = "INSERT INTO codes(code_name) VALUES(%s)" 
+
+# ----- Import Data from csv file -----
+
+# import data from csv file
+with open('codes.csv', newline='') as csvfile:
+     codes = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     codes_list = list(codes)
+     
+# remove first observation in list (column name)
+codes_list = codes_list[1:(len(codes_list)+1)] 
+print(len(codes_list)) 
+
+# format codes values
+codes_list2 = codes_list
+
+for item in range(0,len(codes_list2)):
+    codes_list2[item] = (str(codes_list2[item])[3:6],)
+    
+
+
+# ----- Define function to update Postgresql database -----
+            
+
+# define function to insert all possible unique 3-letter codes into the CROWN database "codes" table
+def import_codes(codes_list):
+
+    sql = "INSERT INTO codes(code) VALUES(%s)" 
     conn = None
     
     try:
@@ -19,7 +51,7 @@ def import_codes(code_list):
         cur = conn.cursor()
         
         # execute the INSERT statement
-        cur.executemany(sql,code_list)
+        cur.executemany(sql,codes_list)
         
         # commit the changes to the database
         conn.commit()
@@ -34,5 +66,9 @@ def import_codes(code_list):
         if conn is not None:
             conn.close()
             
-            
- codes_csv = csv.reader("codes.csv")
+
+
+# ----- Update Postgresql database -----
+
+import_codes(codes_list2)
+     
